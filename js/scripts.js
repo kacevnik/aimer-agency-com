c52498ab9682fd8c4730594a584cd256107b22e4
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-08-25 23:45:11 +0300

    fixbug

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ jQuery(document).ready(function ($) {
     });
 
     //Плавная прокрутка
-    $("a[href*='#']").bind("click", function(e){
-        var anchor = $(this);
-        $('html, body').stop().animate({
-        scrollTop: $(anchor.attr('href')).offset().top
-        }, 500);
-        e.preventDefault();
-        return false;
-    });
+    // $("a[href*='#']").bind("click", function(e){
+    //     var anchor = $(this);
+    //     $('html, body').stop().animate({
+    //     scrollTop: $(anchor.attr('href')).offset().top
+    //     }, 500);
+    //     e.preventDefault();
+    //     return false;
+    // });
 });
